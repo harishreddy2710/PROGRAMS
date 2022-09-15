6de0d5086fe309aefa4922789c8696dd43d40b5c
munugalahari <harishreddy2710@gmail.com>
2022-09-15 17:50:02 +0530

    branch check

--- sample.py
+++ sample.py
@@ -144,11 +144,14 @@
 # print(x//y)
 # print(x % y)
 
+
+
 # 19)Write a program to get unique values from a list.
 # lst = [10,20,30,40,30,50,60,70]
 # st = set(lst)
 # lst1 = list(st)
 # print(lst1)git
+
 
 
 # 20)Write a program to update the value in a dictionary if the key exists.
